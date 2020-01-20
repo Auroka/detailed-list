f27cff2463f08144b06a8a60bd365aff409c0824
Auroka <auroka@163.com>
2020-01-20 11:41:59 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import './App.css';
+import { render } from '@testing-library/react';
+
+
+
+
+// const App: React.FC = () => {
+//   state = { lists: ['1'] };
+//   function addList(v) {
+
+//   }
+//   const { lists } = this.state;
+//   return (
+//     <div className="App">
+//       <ToDoList lists={lists}></ToDoList>
+//       <InputContent addList={toAddList}></InputContent>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  state = { lists: ['1'] };
+  add = (v: string) => {
+    const { lists } = this.state;
+    lists.push(v);
+    this.setState({ lists })
+  }
+  render() {
+    return (
+      <div>
+        <InputContent toAddList={this.add}></InputContent>
+        <ToDoList lists={this.state.lists}></ToDoList>
+      </div>
+    )
+  }
+}
+
+interface ToDoListProps {
+  lists: string[];
+}
+class ToDoList extends React.Component<ToDoListProps> {
+
+  render() {
+    return (
+      <ul>
+        {
+          this.props.lists.map((item, index) =>
+            <li key={index}>{item}</li>
+          )
+        }
+      </ul>
+    )
+  }
+}
+
+interface InputContentProps {
+  toAddList: Function;
+}
+class InputContent extends React.Component<InputContentProps> {
+  enterContent(e: any) {
+    if (e.keyCode === 13) {
+      this.props.toAddList(e.target.value);
+      e.target.value = ''
+    }
+  }
+  render() {
+    return (
+      <input type="text" onKeyUp={(e) => { this.enterContent(e) }} />
+    );
+  }
+}
+
+export default App;
